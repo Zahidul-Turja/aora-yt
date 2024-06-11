a87c5d1e9d05b97a4a9f47bd39d142ac78da1cfb
Zahidul-Turja <zahidulturja@gmail.com>
2024-06-12 00:50:42 +0600

    initial setup

---
+++ app/(auth)/_layout.jsx
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet } from "react-native";
+
+function AuthLayout() {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+}
+
+export default AuthLayout;
+
+const styles = StyleSheet.create({});
---
+++ app/profile.jsx
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet } from "react-native";
+
+function profile() {
+  return (
+    <View>
+      <Text>Profile</Text>
+    </View>
+  );
+}
+
+export default profile;
+
+const styles = StyleSheet.create({});
