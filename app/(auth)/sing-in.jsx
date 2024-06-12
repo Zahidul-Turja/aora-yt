f4e9a1893e6d4815b062a2bc05badef6aeb0a5bd
Zahidul-Turja <zahidulturja@gmail.com>
2024-06-12 10:46:12 +0600

    Tabs done

---
+++ app/(auth)/sing-in.jsx
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet } from "react-native";
+
+function SingIn() {
+  return (
+    <View>
+      <Text>SignIn</Text>
+    </View>
+  );
+}
+
+export default SingIn;
+
+const styles = StyleSheet.create({});
